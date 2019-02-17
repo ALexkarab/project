d3b0471e23c27c57b19a0f71d30d71a31b63f3c1
ALexkarab <sandikakostajik.95@gmail.com>
2019-02-17 15:34:20 +0400

    first commit

---
+++ SimplexMethod/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SimplexMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.matrix = new System.Windows.Forms.DataGridView();
+            this.varCount = new System.Windows.Forms.TextBox();
+            this.restrCount = new System.Windows.Forms.TextBox();
+            this.SetGrid = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SolveSystem = new System.Windows.Forms.Button();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.pastMatrix = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.matrix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pastMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // matrix
+            // 
+            this.matrix.AllowUserToAddRows = false;
+            this.matrix.AllowUserToDeleteRows = false;
+            this.matrix.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.matrix.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.matrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrix.Location = new System.Drawing.Point(415, 17);
+            this.matrix.Name = "matrix";
+            this.matrix.Size = new System.Drawing.Size(289, 150);
+            this.matrix.TabIndex = 0;
+            // 
+            // varCount
+            // 
+            this.varCount.Location = new System.Drawing.Point(155, 17);
+            this.varCount.Name = "varCount";
+            this.varCount.Size = new System.Drawing.Size(40, 20);
+            this.varCount.TabIndex = 1;
+            // 
+            // restrCount
+            // 
+            this.restrCount.Location = new System.Drawing.Point(155, 46);
+            this.restrCount.Name = "restrCount";
+            this.restrCount.Size = new System.Drawing.Size(40, 20);
+            this.restrCount.TabIndex = 2;
+            // 
+            // SetGrid
+            // 
+            this.SetGrid.Location = new System.Drawing.Point(75, 81);
+            this.SetGrid.Name = "SetGrid";
+            this.SetGrid.Size = new System.Drawing.Size(75, 23);
+            this.SetGrid.TabIndex = 3;
+            this.SetGrid.Text = "Задать систему";
+            this.SetGrid.UseVisualStyleBackColor = true;
+            this.SetGrid.Click += new System.EventHandler(this.SetGrid_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Кол-во переменных";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кол-во ограничений";
+            // 
+            // SolveSystem
+            // 
+            this.SolveSystem.Location = new System.Drawing.Point(25, 429);
+            this.SolveSystem.Name = "SolveSystem";
+            this.SolveSystem.Size = new System.Drawing.Size(75, 23);
+            this.SolveSystem.TabIndex = 6;
+            this.SolveSystem.Text = "Решить";
+            this.SolveSystem.UseVisualStyleBackColor = true;
+            this.SolveSystem.Click += new System.EventHandler(this.SolveSystem_Click);
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(132, 382);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logBox.Size = new System.Drawing.Size(147, 108);
+            this.logBox.TabIndex = 7;
+            // 
+            // pastMatrix
+            // 
+            this.pastMatrix.AllowUserToAddRows = false;
+            this.pastMatrix.AllowUserToDeleteRows = false;
+            this.pastMatrix.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.pastMatrix.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.pastMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pastMatrix.Location = new System.Drawing.Point(415, 211);
+            this.pastMatrix.Name = "pastMatrix";
+            this.pastMatrix.ReadOnly = true;
+            this.pastMatrix.Size = new System.Drawing.Size(289, 172);
+            this.pastMatrix.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 514);
+            this.Controls.Add(this.pastMatrix);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.SolveSystem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SetGrid);
+            this.Controls.Add(this.restrCount);
+            this.Controls.Add(this.varCount);
+            this.Controls.Add(this.matrix);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.matrix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pastMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView matrix;
+        private System.Windows.Forms.TextBox varCount;
+        private System.Windows.Forms.TextBox restrCount;
+        private System.Windows.Forms.Button SetGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SolveSystem;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.DataGridView pastMatrix;
+    }
+}
+
---
+++ SimplexMethod/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimplexMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetGrid_Click(object sender, EventArgs e)
+        {
+            //задаем параметры таблицы для отображения
+            matrix.RowCount = int.Parse(restrCount.Text) + 1;
+            matrix.ColumnCount = int.Parse(varCount.Text) + 1;
+            matrix.Height = 20*matrix.RowCount + 15;
+            matrix.RowHeadersWidth = 60;
+
+            //задаем имена рядов
+            for (int i = 0; i < matrix.RowCount-1; i++) 
+            {
+                matrix.Rows[i].HeaderCell.Value = "0=";
+            }
+            matrix.Rows[matrix.RowCount - 1].HeaderCell.Value = "F=";
+
+            //задаем имена колонок
+            matrix.Columns[0].HeaderCell.Value = "1";
+            for (int i = 1; i < matrix.ColumnCount; i++) 
+            {
+                matrix.Columns[i].HeaderCell.Value = "-x" + i;
+            }
+        }
+        private void CopyToPastMatrix() 
+        {
+            pastMatrix.RowCount = matrix.RowCount;
+            pastMatrix.ColumnCount = matrix.ColumnCount;
+            pastMatrix.Height = 20 * pastMatrix.RowCount + 15;
+            pastMatrix.RowHeadersWidth = 60;
+
+            //задаем имена рядов
+            for (int i = 0; i < pastMatrix.RowCount; i++)
+            {
+                pastMatrix.Rows[i].HeaderCell.Value = matrix.Rows[i].HeaderCell.Value;
+            }
+
+            //задаем имена колонок
+            for (int i = 0; i < matrix.ColumnCount; i++)
+            {
+                pastMatrix.Columns[i].HeaderCell.Value = matrix.Columns[i].HeaderCell.Value;
+            }
+
+            for (int i = 0; i < matrix.ColumnCount; i++) 
+            {
+                for (int j = 0; j < matrix.RowCount; j++) {
+                    pastMatrix[i, j].Value = matrix[i, j].Value;
+                }
+            }
+        }
+
+        private void SolveSystem_Click(object sender, EventArgs e)
+        {
+            CopyToPastMatrix();
+            if (!isBasisSolution())
+            {
+                FindBasisSolution();
+            }
+            else {
+                if (!isBasePlan())
+                {
+                    FindBasePlan();
+                }
+                else 
+                {
+                    if (!isOptimized())
+                    {
+                        Optimize();
+                    }
+                }
+            }            
+        }
+
+        private bool isBasisSolution() {
+            bool isBasisSolution = true;
+            if (matrix.ColumnCount != int.Parse(varCount.Text) - int.Parse(restrCount.Text) + 1) 
+            {
+                isBasisSolution = false;
+            }
+            if (isBasisSolution)
+            {
+                MessageLog("Базовое решение");
+            }
+            else 
+            {
+                MessageLog("Нет базового решения");
+            }
+            return isBasisSolution;
+        }
+
+        private void FindBasisSolution() 
+        {
+            int rowNum = -1;
+            int colNum = -1;
+            for (int i = 0; i < matrix.RowCount - 1; i++)
+            {
+                if (matrix.Rows[i].HeaderCell.Value.ToString().Equals("0="))
+                {
+                    rowNum = i;
+                    break;
+                }
+            }
+            if (rowNum >= 0)
+            {
+                for (int i = 1; i < matrix.ColumnCount; i++)
+                {
+                    if (GetValue(matrix[i, rowNum]) > 0)
+                    {
+                        colNum = i;
+                        break;
+                    }
+                }
+                if (colNum >= 0)
+                {
+                    Calculate(rowNum, colNum);
+                    matrix.Columns.RemoveAt(colNum);
+
+                    MessageLog("Жорданово исключение с клеткой[" + rowNum + "," + colNum + "]");
+                }
+
+            }
+        }
+
+        private void FindBasePlan() 
+        {
+            for (int i = 0; i < matrix.RowCount - 1; i++)
+            {
+                if (GetValue(matrix[0, i]) < 0)
+                {
+                    for (int j = 0; j < matrix.ColumnCount; j++)
+                    {
+                        matrix[j, i].Value = -1 * GetValue(pastMatrix[j, i]);
+                    }
+                }
+            }
+        }
+
+        private void Optimize() 
+        {
+            double minColumnVal = 0;//минимальный из элементов целевой функции
+            double minRowVal = 0;//минимальное положительное отношение свободного члена к значению в столбце
+            int colNum = -1;
+            int rowNum = -1;
+            bool elemIsFound = false;
+            for (int i = 1; i < matrix.ColumnCount; i++) {
+                if (GetValue(matrix[i, matrix.RowCount - 1]) < minColumnVal) 
+                {
+                    colNum = i;
+                    minColumnVal = GetValue(matrix[i, matrix.RowCount - 1]);
+                }
+            }
+            for (int i = 0; i < matrix.RowCount - 1; i++) {
+                if (GetValue(matrix[colNum, i]) > 0)
+                {
+                    if (!elemIsFound)
+                    {
+                        elemIsFound = true;
+                        rowNum = i;
+                        minRowVal = GetValue(matrix[0, i]) / GetValue(matrix[colNum, i]);
+                    }
+                    else
+                    {
+                        rowNum = i;
+                        minRowVal = GetValue(matrix[0, i]) / GetValue(matrix[colNum, i]);
+                    }
+                }
+            }
+            if (!elemIsFound)
+            {
+                MessageLog("Нет оптимизации");
+            }
+            Calculate(rowNum, colNum);
+        }
+
+        private bool isBasePlan() 
+        {
+            bool isBasePlan = true;
+            for (int i = 0; i < matrix.RowCount - 1; i++) 
+            {
+                if (GetValue(matrix[0, i]) < 0)
+                {
+                    isBasePlan = false;
+                    break;
+                }
+            }
+            if (isBasePlan)
+            {
+                MessageLog("Опорный план");
+            }
+            else
+            {
+                MessageLog("Нет опорного плана");
+            }
+            return isBasePlan;
+        }
+
+        private bool isOptimized() 
+        {
+            bool isOptimized = true;
+            for (int i = 1; i < matrix.ColumnCount; i++) {
+                if (GetValue(matrix[i, matrix.RowCount - 1]) < 0)
+                {
+                    isOptimized = false;
+                }
+            }
+            if (isOptimized)
+            {
+                MessageLog("Оптимизировано");
+            }
+            else
+            {
+                MessageLog("Не оптимизировано");
+            }
+            return isOptimized;
+        }
+
+        private void Calculate(int rowNum,int colNum) 
+        {
+            for (int i = 0; i < matrix.RowCount; i++) 
+            {
+                for (int j = 0; j < matrix.ColumnCount; j++) 
+                {
+                    if (i == rowNum && j == colNum)
+                    {
+                        matrix[j, i].Value = 1 / GetValue(pastMatrix[colNum, rowNum]);
+                    }
+                    else if (i == rowNum)
+                    {
+                        matrix[j, i].Value = GetValue(pastMatrix[j, i]) / GetValue(pastMatrix[colNum, rowNum]);
+                    }
+                    else if (j == colNum)
+                    {
+                        matrix[j, i].Value = -1*GetValue(pastMatrix[j, i]) / GetValue(pastMatrix[colNum, rowNum]);
+                    }
+                    else
+                    {
+                        matrix[j, i].Value =  (GetValue(pastMatrix[j, i]) * GetValue(pastMatrix[colNum,rowNum]) - GetValue(pastMatrix[j,rowNum])*GetValue(pastMatrix[colNum,i]))/GetValue(pastMatrix[colNum,rowNum]);
+                    }
+                }
+            }
+
+            string colHeaderValue = matrix.Columns[colNum].HeaderCell.Value.ToString();
+            string rowHeaderValue = matrix.Rows[rowNum].HeaderCell.Value.ToString();
+            matrix.Columns[colNum].HeaderCell.Value = "-" + rowHeaderValue;
+            matrix.Rows[rowNum].HeaderCell.Value = colHeaderValue.Substring(1);
+        }
+        private double GetValue(DataGridViewCell cell) 
+        {
+            return Convert.ToDouble(cell.Value);
+        }
+        private void MessageLog(string str) {
+            logBox.Text += Environment.NewLine;
+            logBox.Text += str;
+        }
+
+    }
+}
